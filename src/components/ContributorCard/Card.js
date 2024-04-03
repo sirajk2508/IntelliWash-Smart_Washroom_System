3fb631a796a2db08ff4f638550c6767ab2786173
sirajk2508 <sirajkhan3649@gmail.com>
2024-04-03 21:19:57 +0530

    Added details of other members

--- src/components/ContributorCard/Card.js
+++ src/components/ContributorCard/Card.js
@@ -1,8 +1,8 @@
 import { CONTRIBUTOR_IMAGE_URL } from "../lib/constants";
 
-const Card = () => {
+const Card = ({name, role, image}) => {
   return (
-    <div className="w-64 bg-white text-center p-8 rounded-sm">
+    <div className="w-64 bg-white text-center p-8 rounded-sm mx-4">
       <div className="flex items-center justify-center">
         <img
           src={CONTRIBUTOR_IMAGE_URL}
@@ -11,8 +11,8 @@ const Card = () => {
         />
       </div>
 
-      <h4 className="font-bold text-xl my-1">Name</h4>
-      <p className="font-semibold text-gray-500">Role</p>
+      <h4 className="font-bold text-xl my-1">{name}</h4>
+      <p className="font-semibold text-gray-500">{role}</p>
       <button className="border border-blue-600 my-3 w-36 h-10 rounded-md text-gray-500 font-bold hover:bg-blue-500 hover:text-white">Connect</button>
     </div>
   );
--- src/components/ContributorCard/Contributors.js
+++ src/components/ContributorCard/Contributors.js
@@ -3,8 +3,11 @@ import Card from './Card'
 
 const Contributors = () => {
   return (
-    <div className='my-5 p-10 bg-gray-100'>
-      <Card/>
+    <div className='my-5 p-10 bg-gray-100 flex justify-center items-center'>
+      <Card name="Dr. Gayatri Phade" role="Project Guide"/>
+      <Card name="Siraj Khan" role="Software Developer."/>
+      <Card name="Arshad Khan" role="Hardware Engineer"/>
+      <Card name="Junaid Shaikh" role="Hardware Engineer"/>
     </div>
   )
 }
